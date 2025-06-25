fcc1d5a7366d741fa95c6012ecf625c3becdcd88
Hank <3533482622@qq.com>
2025-06-26 00:04:52 +0800

    fix：新添加依赖包

--- stores/counter.ts
+++ stores/counter.ts
@@ -1,25 +1,25 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
 // 定义状态接口
 interface CounterState {
-  count: number
+    count: number;
 }
 
-export const useCounterStore = defineStore('counter', {
-  state: (): CounterState => ({
-    count: 0
-  }),
-  actions: {
-    increment() {
-      this.count++
+export const useCounterStore = defineStore("counter", {
+    state: (): CounterState => ({
+        count: 0,
+    }),
+    actions: {
+        increment() {
+            this.count++;
+        },
+        decrement() {
+            this.count--;
+        },
     },
-    decrement() {
-      this.count--
-    }
-  },
-  getters: {
-    doubleCount(): number {
-      return this.count * 2
-    }
-  }
-}) 
+    getters: {
+        doubleCount(): number {
+            return this.count * 2;
+        },
+    },
+});
